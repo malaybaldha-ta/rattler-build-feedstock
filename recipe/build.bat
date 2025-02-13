5c2a979bedcac861816157b6514723f11709cd80
Ubuntu <ubuntu@ip-172-31-9-91.ap-southeast-1.compute.internal>
2025-02-13 07:45:39 +0000

    ENH: Fix windows build in workflow try-7.

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,18 +1,13 @@
-@echo on
-:: Set up the MSVC environment
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-:: Create build directory
 mkdir builddir
 
-:: Build the wheel
 %PYTHON% -m build -w -n -x ^
     -Cbuilddir=builddir ^
     -Csetup-args=-Dblas=blas ^
     -Csetup-args=-Dlapack=lapack
 if %ERRORLEVEL% neq 0 exit 1
 
-:: Install the built wheel (workaround for Windows)
+:: `pip install dist\numpy*.whl` does not work on windows,
+:: so use a loop; there's only one wheel in dist/ anyway
 for /f %%f in ('dir /b /S .\dist') do (
     pip install %%f
     if %ERRORLEVEL% neq 0 exit 1
