f11be793a509a4af37465b2fb5e44d3284470b92
Ubuntu <ubuntu@ip-172-31-9-91.ap-southeast-1.compute.internal>
2025-02-13 06:45:16 +0000

    ENH: Fix windows build in workflow try-1.

---
+++ recipe/build.bat
@@ -0,0 +1,14 @@
+mkdir builddir
+
+%PYTHON% -m build -w -n -x ^
+    -Cbuilddir=builddir ^
+    -Csetup-args=-Dblas=blas ^
+    -Csetup-args=-Dlapack=lapack
+if %ERRORLEVEL% neq 0 exit 1
+
+:: `pip install dist\numpy*.whl` does not work on windows,
+:: so use a loop; there's only one wheel in dist/ anyway
+for /f %%f in ('dir /b /S .\dist') do (
+    pip install %%f
+    if %ERRORLEVEL% neq 0 exit 1
+)
