3a222a2c5677991f07a97c86ab0b2aef51db4149
Ubuntu <ubuntu@ip-172-31-9-91.ap-southeast-1.compute.internal>
2025-02-13 07:12:24 +0000

    ENH: Fix windows build in workflow try-4.

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,17 +1,18 @@
 @echo on
-for /f "usebackq tokens=*" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
-    call "%%i\VC\Auxiliary\Build\vcvars64.bat"
-)
+:: Set up the MSVC environment
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
+:: Create build directory
 mkdir builddir
 
+:: Build the wheel
 %PYTHON% -m build -w -n -x ^
     -Cbuilddir=builddir ^
-    -Csetup-args=-Dbackend=ninja ^
     -Csetup-args=-Dblas=blas ^
     -Csetup-args=-Dlapack=lapack
 if %ERRORLEVEL% neq 0 exit 1
 
+:: Install the built wheel (workaround for Windows)
 for /f %%f in ('dir /b /S .\dist') do (
     pip install %%f
     if %ERRORLEVEL% neq 0 exit 1
